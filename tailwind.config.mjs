abe6c6e6627586588c828d9e7a39a21a585e77b9
facubotta <nutuestampados@gmail.com>
2024-05-15 21:47:53 +0200

    skills readys - go to experiences details

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,21 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: 'class',
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        'cyan': '#67e8f9',
+        'magenta-dark': '#ff00ff',
+        'magenta-light': '#f0a4f0',
+        'yellow': '#FACC15'
+      },
+      fontFamily: {
+        'neon': ['Tilt Neon', ...defaultTheme.fontFamily.serif],
+      },
+    },
+  },
+  plugins: [],
 }
